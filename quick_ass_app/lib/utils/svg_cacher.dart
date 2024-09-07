659c1b61a40cda931fbe075e118550bb0c1bb781
yelyseiruban <ruban.elisey.1996@gmail.com>
2024-09-07 20:19:00 +0200

    Svg caching before show ui

---
+++ quick_ass_app/lib/utils/svg_cacher.dart
@@ -0,0 +1,13 @@
+
+class SvgCache {
+  Future<void> cacheIcons() async {
+    for (String iconPath in _svgList) {
+      var loader = SvgAssetLoader(iconPath);
+      await svg.cache.putIfAbsent(loader.cacheKey(null), () => loader.loadBytes(null));
+    }
+  }
+
+  final List<String> _svgList = [
+    'assets/icons/rabbit.svg',
+  ];
+}
